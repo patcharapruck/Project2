84d8de913d77a4be8b4c0d0c2ad348f6a755003a
PCHRP <fluk.fluk.zuza@gmail.com>
2019-06-19 15:11:00 +0700

    กดกลับในแต่ละหน้า

--- app/src/main/java/baby/com/project2/activity/VaccineActivity.java
+++ app/src/main/java/baby/com/project2/activity/VaccineActivity.java
@@ -2,14 +2,36 @@ package baby.com.project2.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import baby.com.project2.R;
 
 public class VaccineActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaccine);
+        initInstances();
+    }
+
+    private void initInstances(){
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
